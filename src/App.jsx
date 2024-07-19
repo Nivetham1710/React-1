fcdce4128d806eafcb967a1010455edac2581e6f
Nivetha. M <94155183+Nivetham1710@users.noreply.github.com>
2024-07-19 15:08:09 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+// src/App.jsx
+import React from 'react';
+import './App.css';
+import Stopwatch from './component/stopwatch';
+
+const App = () => {
+  return (
+    <div className="app">
+      <Stopwatch />
+    </div>
+  );
+};
+
+export default App;
